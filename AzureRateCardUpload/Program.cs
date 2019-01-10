3ce65cff7a04d4621ba3e435a0a01d9f07307763
Florian Wagner (플로리안 와그너) <florian.wagner@bespinglobal.com>
2019-01-10 15:34:44 +0900

    Add project files.

---
+++ AzureRateCardUpload/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureRateCardUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
